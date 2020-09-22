b7743f6a01e30b71b0d195328da958aadadbc320
ibsanju <bharath2668@gmail.com>
2020-09-22 13:15:30 +0530

    InitializeApp with Firebase-core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hot_brew/screens/wrapper.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  // these 2 lines to InitializeApp with Firebase-core
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
