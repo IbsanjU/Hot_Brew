73db0ff214dc6c9fdec03ee4503390ed074e86be
ibsanju <bharath2668@gmail.com>
2020-09-22 22:40:24 +0530

    20 INSERT Data into Firestore (Test data)

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hot_brew/models/user.dart';
+import 'package:hot_brew/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -34,8 +35,8 @@ class AuthService {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-          User user = result.user;
-          return _userFromFirebaseUser(user);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
@@ -48,6 +49,11 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user;
+
+      // Create a new document for the user with the uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new brew member', 100);
+
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
