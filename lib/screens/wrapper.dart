05a123ee3b23104dcec8bb6c046bbeda99c9b829
ibsanju <bharath2668@gmail.com>
2020-09-22 17:44:59 +0530

    07 redirect using StreamProvider

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hot_brew/models/user.dart';
 import 'package:hot_brew/screens/authenticate/authenticate.dart';
+import 'package:hot_brew/screens/home/home.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    // Listen / Access Stream Provider for AuthChange User
     final user = Provider.of<Users>(context);
     print(user);
-        
+
     // return either Home or Authenticate widget
-    return Authenticate();
+    if (user == null)
+      return Authenticate();
+    else
+      return Home();
     // return FirestoreExampleApp();
   }
 }
