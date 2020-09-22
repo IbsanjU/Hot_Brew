ed5d5bfe5e3a4a7754c4632666122f48cb076885
ibsanju <bharath2668@gmail.com>
2020-09-22 17:06:51 +0530

    06 Stream to check for auth changes and reroute wrapper

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,12 +2,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hot_brew/models/user.dart';
 
 class AuthService {
-  
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Create user object on Firebase User
-  Users _userFromFirebaseuser(User user){
-    return user !=null ? Users(uid: user.uid) : null;
+  Users _userFromFirebaseUser(User user) {
+    return user != null ? Users(uid: user.uid) : null;
+  }
+
+// auth change user stream
+  Stream<Users> get user {
+    return _auth
+        .authStateChanges()
+        //.map((User user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
   }
 
 // Sign in anonomously
@@ -16,7 +23,7 @@ class AuthService {
     try {
       UserCredential result = await _auth.signInAnonymously();
       final User user = result.user;
-      return _userFromFirebaseuser(user);
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
