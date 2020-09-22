7ccfd7ac2c6bdb9aff292b9b90b0cbeba8f37e58
ibsanju <bharath2668@gmail.com>
2020-09-22 17:39:57 +0530

    Insert StreamProvider to check for users
    insert Provider Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hot_brew/models/user.dart';
 import 'package:hot_brew/screens/wrapper.dart';
+import 'package:hot_brew/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // these 2 lines to InitializeApp with Firebase-core
@@ -14,8 +17,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    //Wrap MaterialApp with Widget =ie.= Streamprovider to make Provider access everywhere
+    return StreamProvider<Users>.value( // users is what we are listening to value <Users>
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hot_brew/models/user.dart';
 import 'package:hot_brew/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<Users>(context);
+    print(user);
         
     // return either Home or Authenticate widget
     return Authenticate();
