71970c5a76af97b7a10be9ab0fed7140bca0774d
ibsanju <bharath2668@gmail.com>
2020-09-23 03:09:33 +0530

    24 Bottom Sheet initial

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -10,6 +10,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+              child: Text("Bottom Sheet"),
+            );
+          });
+    }
+
     return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
@@ -25,6 +36,10 @@ class Home extends StatelessWidget {
                 },
                 icon: Icon(Icons.person),
                 label: Text('Logout')),
+            FlatButton.icon(
+                onPressed: () => _showSettingsPanel(),
+                icon: Icon(Icons.settings),
+                label: Text('Settings')),
           ],
         ),
         body: BrewList(),
